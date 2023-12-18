eb39d849a94cc0f44ee7cf1250a2cb6a1b1e7a36
Felix Krömer <kroemer.felix@gmail.com>
2023-12-18 23:07:15 +0100

    finish 18 pt 2

--- aoc2023/18/solution_pt2.py
+++ aoc2023/18/solution_pt2.py
@@ -14,6 +14,7 @@ def get_size(steps):
             wMin = pos[1]
         if(pos[1] > wMax):
             wMax = pos[1]
+    print(hMax, hMin, wMax, wMin)
     size = (abs(hMax - hMin) + 1) * (abs(wMax - wMin) + 1)
     if(len(steps) == 5):
         return size
@@ -26,8 +27,14 @@ def get_size(steps):
     s_index = None
     prev = None
     index = 0
+    start_pos = None
+    start_index = None
     for pos in steps: 
         if pos[0] == hMax or pos[0] == hMin or pos[1] == wMax or pos[1] == wMin:
+            print(pos)
+            if not start:
+                start_pos = pos
+                start_index = index
             start = True
             if running:
                 on_edge.append((s_pos, pos))
@@ -40,14 +47,23 @@ def get_size(steps):
                 s_index = index - 1
         prev = pos
         index += 1
+    if running:
+        on_edge.insert(0, (s_pos, start_pos))
+        # (0,0) is not on border, take last and first on border,
+        # subtract len(steps) from index of last element on border, use mod later
+        on_edge_index.insert(0, (s_index - len(steps), start_index + 1))    
     for x, index in zip(on_edge, on_edge_index):
         new_steps = []
         edge = 0
         start = False
+        prev = None
         for i in range(index[0], index[1]):
+            if(i == -1):
+                continue
             if start:
-                edge += abs(steps[i][0] - steps[i-1][0]) + abs(steps[i][1] - steps[i-1][1]) + 1
+                edge += abs(steps[i][0] - steps[prev][0]) + abs(steps[i][1] - steps[prev][1]) + 1
             start = True
+            prev = i
             new_steps.append(steps[i])
         edge -= (len(new_steps) - 2)
         new_steps.append(x[0])
@@ -65,7 +81,7 @@ with open("input") as input:
     for line in lines:
         dir, steps, color = line.split()
         steps = int(steps)
-        
+        #"""
         steps = int(color[2:-2],16)
         dir = None
         if color[-2:-1] == '0':
@@ -76,6 +92,7 @@ with open("input") as input:
            dir = 'L'
         else:
             dir = 'U'
+        #"""
         print(dir, steps)
         pos = (pos[0] + (dirs[dir][0]*steps), pos[1] + (dirs[dir][1]*steps))
         field.append(pos)
