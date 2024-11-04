6341b242a6f76850d926a0c11a4d36939b958078
Felix Krömer <kroemer.felix@gmail.com>
2024-11-04 23:04:51 +0100

    aoc 2022 4

---
+++ aoc2022/src/bin/solution_4_2.rs
@@ -0,0 +1,13 @@
+use scan_fmt::scan_fmt;
+use std::fs::read_to_string;
+
+fn main() {
+    let mut sum = 0;
+    for line in read_to_string("input").unwrap().lines() {
+        let (a, b, c, d) = scan_fmt!(line, "{}-{},{}-{}", u32, u32, u32, u32).unwrap();
+        if c >= a && c <= b || d >= a && d <= b || a >= c && a <= d || b >= c && b <= d {
+            sum += 1;
+        }
+    }
+    println!("{}", sum)
+}
