dde4ba7d9ed08ca68afa5ba6d6a80df726733376
Felix Krömer <kroemer.felix@gmail.com>
2024-11-06 22:49:57 +0100

    aoc 2022 6

---
+++ aoc2022/src/bin/solution_6_2.rs
@@ -0,0 +1,20 @@
+use std::{collections::HashSet, fs::read_to_string};
+
+fn main() {
+    let line = read_to_string("input").unwrap();
+    let mut window = Vec::new();
+    for (i, c) in line.chars().enumerate() {
+        if window.len() < 14 {
+            window.push(c);
+        } else {
+            let mut unique = HashSet::new();
+            if window.iter().all(|c| unique.insert(c)) {
+                println!("{}", i);
+                break;
+            } else {
+                window.remove(0);
+                window.push(c);
+            }
+        }
+    }
+}
