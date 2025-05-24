17b164cfee4613a984cd5a0a2bb72ebe6e713603
Felix Krömer <kroemer.felix@gmail.com>
2025-05-24 13:38:39 +0200

    fix directory structure

--- aoc2021/src/main/java/Day23Part1.java
+++ aoc2021/src/main/java/Day23Part1.java
@@ -1,38 +1,48 @@
 import io.smallrye.mutiny.tuples.Tuple3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class Day23Part1 {
 
+    enum Type {
+        A, B, C, D;
+    }
+
+    record Pod(Type type) {
+    }
+
+    record Key(List<Pod> places, List<Pod> holes, long score) {
+    }
+
+
     public static void main(String[] args) throws Exception {
-        List<String> places = new ArrayList<>();
+        List<Pod> places = new ArrayList<>();
         for (int i = 0; i < 11; i++) {
-            places.add("");
+            places.add(null);
         }
-        //List<String> holes = List.of("b1", "c1", "b2", "d1", "a1", "d2", "c2", "a2");
-        List<String> holes = List.of("c1", "b1", "d1", "d2", "b2", "c2", "a1", "a2");
-        Map<String, Long> cache = new HashMap<>();
-        Map<String, Tuple3<Integer, Integer, Long>> dest = new HashMap<>();
-        dest.put("a", Tuple3.of(0, 4, 1L));
-        dest.put("b", Tuple3.of(1, 5, 10L));
-        dest.put("c", Tuple3.of(2, 6, 100L));
-        dest.put("d", Tuple3.of(3, 7, 1000L));
+        List<Pod> holes = List.of(new Pod(Type.B), new Pod(Type.C), new Pod(Type.B), new Pod(Type.D), new Pod(Type.A), new Pod(Type.D), new Pod(Type.C), new Pod(Type.A));
+        //List<Pod> holes = List.of(new Pod(Type.C), new Pod(Type.B), new Pod(Type.D), new Pod(Type.D), new Pod(Type.B), new Pod(Type.C), new Pod(Type.A), new Pod(Type.A));
+        Map<Key, Long> cache = new HashMap<>();
+        Map<Type, Tuple3<Integer, Integer, Long>> dest = new HashMap<>();
+        dest.put(Type.A, Tuple3.of(0, 4, 1L));
+        dest.put(Type.B, Tuple3.of(1, 5, 10L));
+        dest.put(Type.C, Tuple3.of(2, 6, 100L));
+        dest.put(Type.D, Tuple3.of(3, 7, 1000L));
         System.out.println(solve(places, holes, 0, dest, cache));
     }
 
-    static long solve(List<String> places, List<String> holes, long points, Map<String, Tuple3<Integer, Integer, Long>> dest, Map<String, Long> cache) {
-        if (
-                (holes.get(0).equals("a1") && holes.get(4).equals("a2") || holes.get(0).equals("a2") && holes.get(4).equals("a1")) &&
-                        (holes.get(1).equals("b1") && holes.get(5).equals("b2") || holes.get(1).equals("b2") && holes.get(5).equals("b1")) &&
-                        (holes.get(2).equals("c1") && holes.get(6).equals("c2") || holes.get(2).equals("c2") && holes.get(6).equals("c1")) &&
-                        (holes.get(3).equals("d1") && holes.get(7).equals("d2") || holes.get(3).equals("d2") && holes.get(7).equals("d1"))) {
+    static long solve(List<Pod> places, List<Pod> holes, long points, Map<Type, Tuple3<Integer, Integer, Long>> dest, Map<Key, Long> cache) {
+        if (holes.stream().allMatch(Objects::nonNull) &&
+                holes.get(0).type == Type.A && holes.get(4).type == Type.A &&
+                holes.get(1).type == Type.B && holes.get(5).type == Type.B &&
+                holes.get(2).type == Type.C && holes.get(6).type == Type.C &&
+                holes.get(3).type == Type.D && holes.get(7).type == Type.D
+
+        ) {
             return points;
         } else {
-            var key = String.join(",", places) + "," + String.join(",", holes) + points;
+            var key = new Key(Collections.unmodifiableList(places), Collections.unmodifiableList(holes), points);
             if (cache.containsKey(key)) {
                 return cache.get(key);
             }
@@ -40,15 +50,15 @@ public class Day23Part1 {
             int i = -1;
             for (var hole : holes) {
                 i++;
-                if (i >= 4 && !holes.get(i - 4).isEmpty()) {
+                if (i >= 4 && holes.get(i - 4) != null) {
                     continue;
                 }
                 var idx = 2 * (i % 4) + 2;
-                if (!hole.isEmpty()) {
-                    var destination = dest.get(hole.substring(0, 1));
+                if (hole != null) {
+                    var destination = dest.get(hole.type);
                     if (i < 4) {
                         if (destination.getItem1().equals(i)) {
-                            if (holes.get(i + 4).substring(0, 1).equals(hole.substring(0, 1))) {
+                            if (holes.get(i + 4).type == hole.type) {
                                 continue;
                             }
                         }
@@ -57,16 +67,16 @@ public class Day23Part1 {
                             continue;
                         }
                     }
-                    if (places.get(idx).isEmpty()) {
-                        List<String> newPlaces;
-                        List<String> newHoles;
+                    if (places.get(idx) == null) {
+                        List<Pod> newPlaces;
+                        List<Pod> newHoles;
                         long res;
                         var rIndex = idx + 1;
-                        while (rIndex < 11 && places.get(rIndex).isEmpty()) {
+                        while (rIndex < 11 && places.get(rIndex) == null) {
                             newPlaces = new ArrayList<>(places);
                             newPlaces.set(rIndex, hole);
                             newHoles = new ArrayList<>(holes);
-                            newHoles.set(i, "");
+                            newHoles.set(i, null);
                             res = solve(newPlaces, newHoles, points + destination.getItem3() * ((i < 4 ? 1 : 2) + rIndex - idx), dest, cache);
                             if (res < maxPoints) {
                                 maxPoints = res;
@@ -74,11 +84,11 @@ public class Day23Part1 {
                             rIndex++;
                         }
                         var lIndex = idx - 1;
-                        while (lIndex >= 0 && places.get(lIndex).isEmpty()) {
+                        while (lIndex >= 0 && places.get(lIndex) == null) {
                             newPlaces = new ArrayList<>(places);
                             newPlaces.set(lIndex, hole);
                             newHoles = new ArrayList<>(holes);
-                            newHoles.set(i, "");
+                            newHoles.set(i, null);
                             res = solve(newPlaces, newHoles, points + destination.getItem3() * ((i < 4 ? 1 : 2) + idx - lIndex), dest, cache);
                             if (res < maxPoints) {
                                 maxPoints = res;
@@ -91,14 +101,14 @@ public class Day23Part1 {
             i = -1;
             for (var place : places) {
                 i++;
-                if (!place.isEmpty()) {
-                    var destination = dest.get(place.substring(0, 1));
+                if (place != null) {
+                    var destination = dest.get(place.type);
                     var idx = 2 * (destination.getItem1() % 4) + 2;
                     var start = Math.min(idx, i + 1);
                     var end = Math.max(idx, i - 1);
                     var blocked = false;
                     for (int j = start; j <= end; j++) {
-                        if (!places.get(j).isEmpty()) {
+                        if (places.get(j) != null) {
                             blocked = true;
                             break;
                         }
@@ -106,9 +116,9 @@ public class Day23Part1 {
                     if (blocked) {
                         continue;
                     }
-                    if (holes.get(destination.getItem1()).isEmpty() && holes.get(destination.getItem2()).isEmpty()) {
+                    if (holes.get(destination.getItem1()) == null && holes.get(destination.getItem2()) == null) {
                         var newPlaces = new ArrayList<>(places);
-                        newPlaces.set(i, "");
+                        newPlaces.set(i, null);
                         var newHoles = new ArrayList<>(holes);
                         newHoles.set(destination.getItem2(), place);
                         var res = solve(newPlaces, newHoles, points + destination.getItem3() * (Math.abs(idx - i) + 2), dest, cache);
@@ -116,11 +126,11 @@ public class Day23Part1 {
                             maxPoints = res;
                         }
                     } else {
-                        if (!holes.get(destination.getItem2()).substring(0, 1).equals(place.substring(0, 1))) {
+                        if (holes.get(destination.getItem2()).type != place.type) {
                             continue;
                         }
                         var newPlaces = new ArrayList<>(places);
-                        newPlaces.set(i, "");
+                        newPlaces.set(i, null);
                         var newHoles = new ArrayList<>(holes);
                         newHoles.set(destination.getItem1(), place);
                         var res = solve(newPlaces, newHoles, points + destination.getItem3() * (Math.abs(idx - i) + 1), dest, cache);
